9f7d85cc2c44437b02c9e1ece95021e907feeb0f
Mohammadreza Osouli <mohammadreza.osouli@gmail.com>
2020-02-15 03:52:44 +0330

    Add basic files

---
+++ P5. CGI/main.py
@@ -0,0 +1,56 @@
+# Basic OBJ file viewer. needs objloader from:
+#  http://www.pygame.org/wiki/OBJFileLoader
+# LMB + move: rotate
+# RMB + move: pan
+# Scroll wheel: zoom in/out
+import sys, pygame
+import numpy as np
+from pygame.locals import *
+from pygame.constants import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from obj_loader import Obj
+import cv2
+import math
+
+pygame.init()
+viewport = (800,800)
+pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+gluPerspective(90, (viewport[0]/viewport[1]), 0.1, 500.0)
+glMatrixMode(GL_MODELVIEW)
+
+obj = Obj("models/porshe.obj")
+camera_up = np.array([0, 1, 0])
+
+glEnable(GL_CULL_FACE);  
+glCullFace(GL_BACK);  
+
+
+glEnable(GL_LIGHTING)
+glEnable(GL_LIGHT0)
+glLightfv(GL_LIGHT0, GL_POSITION, (0, 20, 0, 1))
+# glMaterialfv(GL_FRONT, GL_AMBIENT, (0, 0.8, 0.8, 1))
+glShadeModel(GL_FLAT)
+
+glEnable(GL_DEPTH_TEST)
+
+
+steps = 20
+distance_range = range(20, 22, 2)
+
+
+for distance in distance_range:
+    for i in range(steps+1):
+        camera_position = np.array([math.cos(i/steps * 2 * math.pi), 0.5, math.sin(i /steps * 2 * math.pi)]) * distance
+        glLoadIdentity()
+        gluLookAt(*camera_position, *obj.position, *camera_up)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        obj.draw()
+        pygame.display.flip()
+        pygame.time.wait(10)
+        screen = glReadPixels(0, 0, *viewport, GL_BGR, type=GL_FLOAT)
+        screen = (screen * 255).astype(int)[::-1]
+        cv2.imwrite("output/%d-%d.png" % (distance, i), screen)
+    
---
+++ P5. CGI/obj_loader.py
@@ -0,0 +1,89 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import numpy as np 
+
+
+class Obj:
+    def __init__(self, file_name):
+        self.vertices = [ ]
+        self.faces = []
+        self.face_normals = []
+        self.normals = []
+        self.position = np.array([0, 0, 0])
+
+        with open(file_name) as handler:
+            for line in handler:
+                instruction = line.split()
+                if len(instruction) == 0 or instruction[0] == "#":
+                    continue
+                if instruction[0] == "v":
+                    vert = np.array(list(map(float, instruction[1:])))
+                    self.vertices.append(vert)
+                
+                elif instruction[0] == "vn":
+                    normal = np.array(list(map(float, instruction[1:])))
+                    self.normals.append(normal)
+                
+                elif instruction[0] == "f":
+                    face = tuple(map(lambda x: int(x.split("/")[0]) - 1, instruction[1:]) )
+                    face_normal = tuple(map(lambda x: int(x.split("/")[-1]) - 1, instruction[1:]) )
+                    self.faces.append(face)
+                    self.face_normals.append(face_normal)
+
+    def draw(self):
+        for normals, face in zip(self.face_normals, self.faces):
+            glBegin(GL_TRIANGLES if len(face) == 3 else GL_QUADS)
+            glNormal3fv( sum(map(lambda x: self.normals[x], normals)) / len(normals) )
+            for vertex_id in face:
+                glVertex3fv(self.vertices[vertex_id] + self.position)
+            glEnd()
+
+    def export_faces(self):
+        return list(map(lambda x: np.array(list(map(lambda y: self.vertices[y], x))), self.faces))
+        
+
+
+if __name__ == "__main__":
+    pass
+    pygame.init()
+    display = (1280,720)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 500.0)
+
+    car = Obj("models/porshe.obj")
+    car.position = np.array([0, 0, 0])
+    camera_position = np.array([0, 0, -50])
+    camera_up = np.array([0, 1, 0])
+    gluLookAt(*camera_position, *car.position, *camera_up)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    glTranslatef(-0.5,0,0)
+                if event.key == pygame.K_RIGHT:
+                    glTranslatef(0.5,0,0)
+
+                if event.key == pygame.K_UP:
+                    glTranslatef(0,1,0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0,-1,0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    glTranslatef(0,0,1.0)
+
+                if event.button == 5:
+                    glTranslatef(0,0,-1.0)
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        car.draw()
+        pygame.display.flip()
+        pygame.time.wait(10)
